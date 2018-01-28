f139cc30b31cf466a45708228b9ad7cb50a9fabe
avanegasp <arco527@gmail.com>
2018-01-28 10:32:13 -0500

    Seudocode to bj

---
+++ bj.rb
@@ -0,0 +1,19 @@
+=begin
+1. Repartir las cartas.
+2. Preguntar al usuario si desea carta o se planta.
+   Si pide carta
+    si se pasa, ir al punto 4
+    De lo contrario (si sigue), ir al punto 2
+   Si se plata, ir la punto 3
+3. Determinar el valor que tiene el repartidor
+    si es menor a 17, entregarse otra carta
+    si se pasa ir al punto 4
+    de lo contrario ir al punto 3
+    de lo contrario ir al punto 4
+4. Determina el ganador.
+    si el usario se paso, gana el repartido
+    de lo contario gana el usuario
+    si el usuario tiene más que el repartidor, gana el usuario
+    de lo contario gana el repartidor
+5. Volver al punto 1 (nuevo juego).
+=end
